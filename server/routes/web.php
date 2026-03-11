a742c14efecc4f4e4dfed0a3189ac1f2ad264ddb
Jacqueline Dávila <a23311069@uthermosillo.edu.mx>
2026-03-10 17:24:14 -0700

    feat(inv): agregar control de stock mínimo

--- server/routes/web.php
+++ server/routes/web.php
@@ -4,39 +4,37 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
- use App\Livewire\Repairs\RepairsList;
+use App\Livewire\Repairs\RepairsList;
 use App\Http\Controllers\InventoryController;
 
-Route::get('/auth/login',[AuthController::class, 'login'])->name('login');
-Route::post('/auth/store',[AuthController::class, 'store'])->name('auth.store');
-Route::post('/auth/logout',[AuthController::class, 'logout'])->name('logout');
+Route::get('/auth/login', [AuthController::class, 'login'])->name('login');
+Route::post('/auth/store', [AuthController::class, 'store'])->name('auth.store');
+Route::post('/auth/logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::middleware('auth')->group(function(){
+Route::middleware('auth')->group(function () {
 
     # --- Home Section --- #
-    Route::get('/',[HomeController::class, 'home'])->name('home');
+    Route::get('/', [HomeController::class, 'home'])->name('home');
     # --- End Home Section --- #
 
     # --- User Section --- #
-    Route::get('/users',[UserController::class, 'index'])->name('users');
+    Route::get('/users', [UserController::class, 'index'])->name('users');
     # --- End User Section --- #
 
     # --- Inventory Section --- #
     Route::get('/inventory', [InventoryController::class, 'index'])->name('inventory.index');
     Route::get('/inventory/create', [InventoryController::class, 'create'])->name('inventory.create');
     Route::post('/inventory', [InventoryController::class, 'store'])->name('inventory.store');
+    Route::get('/inventory/{id}/edit', [InventoryController::class, 'edit'])->name('inventory.edit');
+    Route::put('/inventory/{id}', [InventoryController::class, 'update'])->name('inventory.update');
     Route::get('/inventory/restock', [InventoryController::class, 'restock'])->name('inventory.restock');
     Route::post('/inventory/restock', [InventoryController::class, 'storeRestock'])->name('inventory.storeRestock');
-    Route::get('/inventory/{id}/edit-price', [InventoryController::class, 'edit'])->name('inventory.edit');
-    Route::patch('/inventory/{id}/price', [InventoryController::class, 'updatePrice'])->name('inventory.updatePrice');
-    Route::get('/inventory/{id}/adjust-stock', [InventoryController::class, 'adjustStock'])->name('inventory.adjustStock');
-    Route::patch('/inventory/{id}/adjust-stock', [InventoryController::class, 'storeAdjustStock'])->name('inventory.storeAdjustStock');
 
     #-- Repairs section --#
     Route::get('/repairs', RepairsList::class)->name('repairs.index');
 
 
 
-    
+
     # --- End Inventory Section --- #
 });
