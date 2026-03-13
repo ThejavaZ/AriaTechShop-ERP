01b91b193e9c955851758196a99fc8ad86169b28
TheJavaZ <jsdash10000@gmail.com>
2026-03-13 12:07:42 -0700

    feat(backend): implementar modelos, migraciones y API para productos y categorías

--- server/routes/api.php
+++ server/routes/api.php
@@ -3,11 +3,20 @@
 use App\Http\Controllers\api\UserController as ApiUserController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
+use App\Http\Controllers\api\ProductController;
+use App\Http\Controllers\api\CategoryController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SaleController;
 
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{slug}', [ProductController::class, 'show']);
+Route::get('/categories', [CategoryController::class, 'index']);
+
+
 Route::middleware('auth:sanctum')->group(function(){
-    Route::resource('/', ApiUserController::class);
-    Route::apiResource('sales', SaleController::class);
+
+
+    Route::get('/users', [ApiUserController::class, 'index']);
+    Route::apiResource('/sales', SaleController::class);
 
 });
