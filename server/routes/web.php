6a12c5cdae2cb6b013f1cbd3d6910038f12f3008
Brayan López Orci <a23311020@uthermosillo.edu.mx>
2026-03-13 21:59:09 -0700

    fix: resolver conflictos con main

--- server/routes/web.php
+++ server/routes/web.php
@@ -54,9 +54,16 @@ Route::middleware('auth')->group(function () {
     #-- sales section --#
 
 Route::resource('sales', SaleController::class);
+
 Route::get('/sales-chart-data', [SaleController::class, 'salesChart'])
     ->name('sales.chart.data');
+    
 Route::get('/sales-chart', [SaleController::class, 'chartView'])->name('sales.chart');
+
+
+
+// Route::get('/sales', [SaleController::class, 'index'])->name('sales.index');
+// Route::get('/sales', [SaleController::class, 'index']);
 
 });
 
