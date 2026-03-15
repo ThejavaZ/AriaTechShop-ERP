56c8d6dd512c1150653bb79376f3c887233f8060
Brayan López Orci <a23311020@uthermosillo.edu.mx>
2026-03-14 18:55:24 -0700

    api produccion

--- server/config/cors.php
+++ server/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000',
+    'allowed_origins' => [
                          'https://ariatechshop.netlify.app', 
                       ],
     'supports_credentials' => true,        // si necesitas cookies
