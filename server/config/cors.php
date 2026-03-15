6b9497fdc5ca74fcb8e33a69da87c0989865149e
Brayan López Orci <a23311020@uthermosillo.edu.mx>
2026-03-14 18:42:28 -0700

    Actualizar CORS: permitir frontend en Netlify y localhost

--- server/config/cors.php
+++ server/config/cors.php
@@ -19,8 +19,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
-
+    'allowed_origins' => ['http://localhost:3000',
+                         'https://ariatechshop.netlify.app', 
+                      ],
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
